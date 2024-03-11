7303e8a4d2d87cece636c90ef376624759bb0239
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2024-03-11 09:27:50 -0500

    clean

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import FastAPI
 from app.controllers import refactor_controller
-from instrumentalization import traces
+from instrumentation import traces
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI(title='fastAPI self training', version = '1.0.0', root_path = '')
