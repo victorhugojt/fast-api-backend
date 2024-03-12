869479341d838250399f9e120908841ab0fe946f
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2024-03-12 15:13:30 -0500

    clean

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import os
 from fastapi import FastAPI
 from app.controllers import basic_controller, greetings_controller, coins_controller
-from app.instrumentation import traces
+from app.instrumentation import traces_exporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI(title='fastAPI self training', version = '1.0.0', root_path = '')
 
-tracer = traces.config('greeting-service', os.environ.get("MODE", "otlp-grpc"))
+tracer = traces_exporter.config('greeting-service', os.environ.get("MODE", "otlp-grpc"))
 
 FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
 
